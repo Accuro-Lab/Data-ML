ab4984129e301dad61f81754067130959857e1fd
Nana Kwame Owusu <s.owusu45@gmail.com>
2020-06-19 23:24:14 -0500

    updating ingest with vars

--- ingest.py
+++ ingest.py
@@ -4,12 +4,12 @@ import sys
 import argparse
 
 def ingest(DBName: str, TableName: str, TSVFilename: str) -> None:
-    with open('test.tsv') as tsvfile:
+    with open(TSVFilename) as tsvfile:
         reader = csv.DictReader(tsvfile, dialect='excel-tab')
         for row in reader:
             print('Inserting row: ', end=' ')
             print(row)
-            db.insert('TestTable', 'testdb', row)
+            db.insert(TableName, DBName, row)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest Args Handler')
