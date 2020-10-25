1ba1f6328851890c12d61546e0cf559a09d0d921
gracegimon <gracegimon@gmail.com>
2020-10-25 19:13:00 +0100

    Added documentation to the haystack api
    Added documentation to haystack api file, containing more details about implementation and applied some PEP8 guidelines

--- ml/haystack_api.py
+++ ml/haystack_api.py
@@ -5,8 +5,9 @@ from haystack.reader.farm import FARMReader
 from haystack.reader.transformers import TransformersReader
 from haystack.utils import print_answers
 from haystack.document_store.memory import InMemoryDocumentStore
+from haystack.retriever.sparse import TfidfRetriever
 
-
+import pathlib
 import pandas as pd
 import io
 import re
@@ -14,29 +15,56 @@ import numpy
 
 import os
 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-##### HAYSTACK (same as our Colab Proof of concept)
+
+# HAYSTACK (same as our Colab Proof of concept)
 PROJECT_DIRECTORY = os.getcwd()
-ARTICLES_FOLDER = PROJECT_DIRECTORY+"/data/CKB-articles-scrape/"
+ARTICLES_FOLDER = PROJECT_DIRECTORY + "/data/CKB-articles-scrape/"
 
 # In the bash we have to set export MKL_SERVICE_FORCE_INTEL=1
 
-import pathlib
-from haystack.retriever.sparse import TfidfRetriever
 
-def load_documents_for_reader():
+def load_documents():
+    """Retrieves scraped articles filepaths from ARTICLES_FOLDER
+
+    Returns
+    -------
+    articles: List of str
+      List of filepaths for each article from ARTICLES_FOLDER
+    """
     
     # define the path
     path_folder = pathlib.Path(ARTICLES_FOLDER)
 
-    # # define the pattern
+    # define the pattern
     json_files = "*.json"
+    
+    # extract absolute filepath for each of the articles
+    articles = [
+        json_file.absolute().as_posix() for json_file in path_folder.glob(json_files)
+    ]
 
-    articles = [ json_file.absolute().as_posix() for json_file in path_folder.glob(json_files)]
-   
     return articles
 
+
 def process_documents(articles):
+    """Process articles  and returns it in dictionary format
+
+    Parameters
+    ----------
+    articles : list of str
+        List of articles file path
+
+    Returns
+    -------
+    dicts_textContent : dict
+        Dictionary containing the required structure used by the models:
+        {"text": article text content
+         "meta": {"name": article title, "url": article url}}
+    """
+
     dicts_textContent = []
 
     for article in articles:
@@ -45,37 +73,69 @@ def process_documents(articles):
         except:
             print(article)
             continue
-        complete_text = df_article['textContent'] + df_article['excerpt'] + df_article['title']
-        list_textContent = complete_text.tolist() 
+            
+        # Join textContent excerpt and title as they all provide
+        # interesting information
+        complete_text = (
+            df_article["textContent"] + df_article["excerpt"] + df_article["title"]
+        )
+        list_textContent = complete_text.tolist()
 
+        # For each of the texts format in form of dictionary
         for i in range(len(list_textContent)):
-            dicts_textContent.append({'text': df_article['textContent'][i], 'meta': {'name': df_article['title'][i], 'url': df_article['url'][i]}})
-    
+            dicts_textContent.append(
+                {
+                    "text": df_article["textContent"][i],
+                    "meta": {
+                        "name": df_article["title"][i],
+                        "url": df_article["url"][i],
+                    },
+                }
+            )
+
     return dicts_textContent
 
-def feed_documents_to_model():
+
+def feed_documents_to_model(model_name="deepset/roberta-base-squad2-covid"):
+    """Feeds documents to model and returns a model ready to make predictions
+
+    Parameters
+    ----------
+    model_name : str
+        The path of the model to be selected from HuggingFace
+        By default uses the pretrained version of roBERTa in squad2
+        and covid articles
+
+    Returns
+    -------
+    finder
+        the model to use for predictions
+    """
+
+    # Initialize in memory Document Store
     document_store = InMemoryDocumentStore()
-    articles = load_documents_for_reader()
+    # Load articles and format it as dictionary
+    articles = load_documents()
     dicts_textContent = process_documents(articles)
+    # Store the dictionary with articles content in the Document Store
     document_store.write_documents(dicts_textContent)
+    # Retriever chooses what is the subset of documents that are relevant
+    # many techniques are possible: for dev purposes TfidfRetriever is faster
     retriever = TfidfRetriever(document_store=document_store)
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2-covid", use_gpu=False)
+    # Reader provides interface to use the pre trained transformers
+    # by default we're using the roberta
+    reader = FARMReader(model_name_or_path=model_name, use_gpu=False)
+    # The finder retrieves predictions
     finder = Finder(reader, retriever)
-    
+
     return finder
 
 
-
-### FASTAPI
-
-
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-
+# FASTAPI
 class Input(BaseModel):
-    question : str
-        
+    question: str
+
+
 app = FastAPI()
 
 # Should only execute at moment of load
@@ -83,18 +143,35 @@ finder = feed_documents_to_model()
 
 
 @app.put("/predict")
-def answer_question(d:Input):
-    '''Given a question at input, provide answer using the finder model
-     d: d.question str
-     returns question provided by user
-             answer text as answer
-             score of answer
-             probability of answer
-     '''
-    prediction = finder.get_answers(question=d.question, top_k_retriever=3, top_k_reader=1)
-    answer = prediction['answers'][0]['answer']
-    probability = prediction['answers'][0]['probability']
-    score = prediction['answers'][0]['score']
-    return {'question': d.question, 'answer': answer, 'score': score,'probability': probability}
+def answer_question(d: Input):
+    """Given a question at input, provide answer using the finder model
 
+    Parameters
+    ----------
+    d: d.question str
 
+    Returns
+    -------
+    a dict: {question: provided by user,
+            answer: text as answer,
+            score: of answer,
+            probability: of answer}
+    """
+    
+    # Get predictions for the input question
+    # TODO: Clean question text before passing
+    # it to the model 
+    prediction = finder.get_answers(
+        question=d.question, top_k_retriever=3, top_k_reader=1
+    )
+    # TODO: Filter out the answer if it is not reliable
+    answer = prediction["answers"][0]["answer"]
+    probability = prediction["answers"][0]["probability"]
+    score = prediction["answers"][0]["score"]
+    
+    return {
+        "question": d.question,
+        "answer": answer,
+        "score": score,
+        "probability": probability,
+    }
